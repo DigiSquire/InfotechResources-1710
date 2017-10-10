1d4abea11742e724fde093418f096943c468ba8b
Nida Memon <Nida.Memon@viavisolutions.com>
2017-10-10 20:49:10 +0530

    contact us map

--- js/contact-us.js
+++ js/contact-us.js
@@ -16,4 +16,19 @@ jQuery(document).ready(function($){
 	function checkVal(inputField) {
 		( inputField.val() == '' ) ? inputField.prev('.cd-label').removeClass('float') : inputField.prev('.cd-label').addClass('float');
 	}
+	 /* Google Map */
+		 
+         $('#my-address').gMap({
+            zoom: 5,
+            scrollwheel: true,
+            maptype: 'ROADMAP',
+            markers:[
+            {
+            address: "440/1,Chitrakoot Appartments,Chatushringi Road, Model Colony, Shivajinagar, Pune, Maharashtra 411053, India",  /* You can change your address from here */
+            html: "<b>Address</b>: <br>103,Chitrakoot,Chatushringi Road,Model Colony, Pune,Maharashtra 411053",   /* You can change display address text from here */
+            popup: true
+            }
+            ]
+            });
+              
 });
