a6d8e27d877b3f87c644360cdeb452e0f8a9bd8e
Nida Memon <Nida.Memon@viavisolutions.com>
2017-10-11 00:40:12 +0530

    adding ajax

--- sendEmail.php
+++ sendEmail.php
@@ -1,28 +1,28 @@
 <?php
 
-if(isset($_POST['cd-name'])){
-    $name = $_POST['cd-name'];
+if(isset($_POST['name'])){
+    $name = $_POST['name'];
 }
-if(isset($_POST['cd-email'])){
-    $from = $_POST['cd-email'];
+if(isset($_POST['email'])){
+    $from = $_POST['email'];
 }
-if(isset($_POST['cd-company'])){
-    $companyName = $_POST['cd-company'];
+if(isset($_POST['companyName'])){
+    $companyName = $_POST['companyName'];
 }
 if(isset($_POST['product'])){
     $product = $_POST['product'];
 }
-if(isset($_POST['cd-textarea'])){
-    $formMessage = $_POST['cd-textarea'];
+if(isset($_POST['formMessage'])){
+    $formMessage = $_POST['formMessage'];
 }
-if(isset($_POST['cd-phone'])){
-    $formMessage = $_POST['cd-phone'];
+if(isset($_POST['workPhone'])){
+    $workPhone = $_POST['workPhone'];
 }
 
 $subject = 'Infotech form';
 
-$headers = "From: " . $_POST['cd-email'] . "\r\n";
-$headers .= "Reply-To: ". $_POST['cd-email'] . "\r\n";
+$headers = "From: " . $_POST['email'] . "\r\n";
+$headers .= "Reply-To: ". $_POST['email'] . "\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
@@ -30,17 +30,16 @@ $message = '<html><body style="margin:0;padding:0;font-family:"helvetica";">';
 $message .= '<h1 style="color:#555;margin-left:31%;"><img src="http://www.digisquire.co/images/favicon/logo.png" alt="digisquire logo"></h1>';
 $message .= '<h2>Enquiry form</h2>';
 $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
-$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . $_POST['cd-name'] . "</td></tr>";
-$message .= "<tr><td><strong>Email:</strong> </td><td>" . $_POST['cd-email']. "</td></tr>";
-$message .= "<tr><td><strong>Company Name:</strong> </td><td>" . $_POST['cd-company'] . "</td></tr>";
-$message .= "<tr><td><strong>Work Phone:</strong> </td><td>" . $_POST['cd-phone'] . "</td></tr>";
+$message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . $_POST['name'] . "</td></tr>";
+$message .= "<tr><td><strong>Email:</strong> </td><td>" . $_POST['email']. "</td></tr>";
+$message .= "<tr><td><strong>Company Name:</strong> </td><td>" . $_POST['companyName'] . "</td></tr>";
+$message .= "<tr><td><strong>Work Phone:</strong> </td><td>" . $_POST['workPhone'] . "</td></tr>";
 $message .= "<tr><td><strong>Product:</strong> </td><td>" . $_POST['product'] . "</td></tr>";
-$message .= "<tr><td><strong>Product Quantity:</strong> </td><td>" . $_POST['radio-button'] . "</td></tr>";
-$message .= "<tr><td><strong>Message:</strong> </td><td>" . $_POST['cd-textarea'] . "</td></tr>";
+$message .= "<tr><td><strong>Message:</strong> </td><td>" . $_POST['formMessage'] . "</td></tr>";
 $message .= "</table>";
 $message .= "</body></html>";
 
-$to  = 'mktg.itresources@gmail.com';//replace with your email
+$to  = 'nidamemon29@gmail.com';//replace with your email
 
 $success = "
 <div class=\"row-fluid\">
